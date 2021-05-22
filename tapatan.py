a7800a04a60c94f19e8eeb5de17f75206f796a7e
clement coste <clement.coste.rouet@free.fr>
2021-05-22 14:53:26 +0200

    changement

--- tapatan.py
+++ tapatan.py
@@ -1,15 +1,5 @@
-####################################################
-# groupe MPCI 7
-# Clément Coste
-# Tom Le Goueff
-# Yanis Kbayli
-# Maeva Laurence
-#https://github.com/uvsq22008945/tapatan.py.git
-####################################################
-
-#########################
-# import des modules
 import tkinter as tk
+import tkinter.messagebox
 
 ###########################
 # constantes du programme
@@ -18,21 +8,129 @@ LARGEUR = 600
 HAUTEUR_LIGNE = 400
 LARGEUR_LIGNE = 400
 RAYON = 10
-color = 'black'
+cpt = 0
+color = ['red', 'blue']
+compteur = 0
+
+liste = [
+    ['black', "black", "black"],
+    ['black', "black", "black"],
+    ['black', "black", "black"]
+]
+
+liste_deplacement = [[], [], [], [], [], [], [], [], []]
+
+
 ##########################
 # fonctions du programme
-def couleur_cercle(cercle):
-    global color
-    if color == 'black':
-        color = 'blue'
-    else:
-        color = 'black'
-    canvas.itemconfig(cercle, fill=color)
+def couleur_cercle(cercle, x, y):
+    global color, cpt, liste, compteur
+    if liste[x][y] == "black" and compteur < 6:
+        cpt = (cpt + 1) % 2
+        canvas.itemconfig(cercle, fill=color[cpt])
+        liste[x][y] = color[cpt]
+        if tableau() and compteur != 6:
+            gagnant = liste[x][y]
+            tkinter.messagebox.showinfo("success", gagnant+" Success unlocked")
+        compteur += 1
+    if compteur == 6:
+        deplacement()
+        print(liste_deplacement)
+
+
+def tableau():
+
+    if (    liste[0][0] == liste[0][1] == liste[0][2] != 'black' or # première ligne
+            liste[1][0] == liste[1][1] == liste[1][2] != 'black' or # deuxième ligne
+            liste[2][0] == liste[2][1] == liste[2][2] != 'black' or # troisième ligne
+            liste[0][0] == liste[1][0] == liste[2][0] != 'black' or # première colonne
+            liste[0][1] == liste[1][1] == liste[2][1] != 'black' or # deuxième colonne
+            liste[0][2] == liste[1][2] == liste[2][2] != 'black' or # troisième colonne
+            liste[0][0] == liste[1][1] == liste[2][2] != 'black' or # première diagonale
+            liste[2][0] == liste[1][1] == liste[0][2] != 'black' # deuxième diagonale
+        ): 
+        return True
+    return False
+
+
+def deplacement():
+
+    if liste[0][0] != "black":
+        deplacement_append(
+            0, 0, 
+            (liste[0][1], 2),
+            (liste[1][0], 4),
+        )
+    if liste[0][1] != "black":
+            deplacement_append(
+            0, 1, 
+            (liste[0][0], 1),
+            (liste[0][2], 3),
+        )
+    if liste[0][2] != "black":
+            deplacement_append(
+            0, 2, 
+            (liste[0][1], 2),
+            (liste[0][2], 3),
+        )
+    if liste[1][0] != "black":
+            deplacement_append(
+            1, 0, 
+            (liste[0][0], 1),
+            (liste[2][0], 7),
+        )
+    if liste[1][1] != "black":
+            deplacement_append(
+            1, 1, 
+            (liste[0][0], 1),
+            (liste[0][1], 2),
+            (liste[0][2], 3),
+            (liste[1][0], 4),
+            (liste[1][0], 6),
+            (liste[2][0], 7),
+            (liste[2][1], 8),
+            (liste[2][2], 9)
+        )
+    if liste[1][2] != "black":
+            deplacement_append(
+            1, 2, 
+            (liste[0][2], 3),
+            (liste[2][2], 9)
+        )
+    if liste[2][0] != "black":
+            deplacement_append(
+            2, 0, 
+            (liste[1][0], 4),
+            (liste[2][0], 7)
+        )
+    if liste[2][1] != "black":
+            deplacement_append(
+            2, 1, 
+            (liste[2][0], 7),
+            (liste[2][2], 9)
+        )
+    if liste[2][2] != "black":
+            deplacement_append(
+            2, 2, 
+            (liste[1][2], 6),
+            (liste[2][1], 8),
+        )
+
+
+def deplacement_append(i, j, *args):
+    for argument in args:
+        if argument[0] == "black":
+            liste_deplacement[i*3+j].append(argument[1])
+    if liste[1][1] == "black":
+        liste_deplacement[i*3+j].append(5)
+
+
 
 #############################
 # programme principal
 racine = tk.Tk()
 canvas = tk.Canvas(racine, width=LARGEUR, height=HAUTEUR)
+canvas.grid()
 
 # plateau
 canvas.create_rectangle(100, 100, 500, 500, fill="#D3D2A5")
@@ -47,22 +145,26 @@ cercle_1 = canvas.create_oval(100 - RAYON, 100 - RAYON, 100 + RAYON, 100 + RAYON
 cercle_2 = canvas.create_oval(300 - RAYON, 100 - RAYON, 300 + RAYON, 100 + RAYON, fill="black")
 cercle_3 = canvas.create_oval(500 - RAYON, 100 - RAYON, 500 + RAYON, 100 + RAYON, fill="black")
 cercle_4 = canvas.create_oval(100 - RAYON, 300 - RAYON, 100 + RAYON, 300 + RAYON, fill="black")
-cercle_5 = canvas.create_oval(100 - RAYON, 500 - RAYON, 100 + RAYON, 500 + RAYON, fill="black")
-cercle_6 = canvas.create_oval(300 - RAYON, 300 - RAYON, 300 + RAYON, 300 + RAYON, fill="black")
-cercle_7 = canvas.create_oval(300 - RAYON, 500 - RAYON, 300 + RAYON, 500 + RAYON, fill="black")
-cercle_8 = canvas.create_oval(500 - RAYON, 300 - RAYON, 500 + RAYON, 300 + RAYON, fill="black")
+cercle_5 = canvas.create_oval(300 - RAYON, 300 - RAYON, 300 + RAYON, 300 + RAYON, fill="black")
+cercle_6 = canvas.create_oval(500 - RAYON, 300 - RAYON, 500 + RAYON, 300 + RAYON, fill="black")
+cercle_7 = canvas.create_oval(100 - RAYON, 500 - RAYON, 100 + RAYON, 500 + RAYON, fill="black")
+cercle_8 = canvas.create_oval(300 - RAYON, 500 - RAYON, 300 + RAYON, 500 + RAYON, fill="black")
 cercle_9 = canvas.create_oval(500 - RAYON, 500 - RAYON, 500 + RAYON, 500 + RAYON, fill="black")
 
-canvas.tag_bind(cercle_1, "<1>", lambda cercle: couleur_cercle(cercle_1))
-canvas.tag_bind(cercle_2, "<1>", lambda cercle: couleur_cercle(cercle_2))
-canvas.tag_bind(cercle_3, "<1>", lambda cercle: couleur_cercle(cercle_3))
-canvas.tag_bind(cercle_4, "<1>", lambda cercle: couleur_cercle(cercle_4))
-canvas.tag_bind(cercle_5, "<1>", lambda cercle: couleur_cercle(cercle_5))
-canvas.tag_bind(cercle_6, "<1>", lambda cercle: couleur_cercle(cercle_6))
-canvas.tag_bind(cercle_7, "<1>", lambda cercle: couleur_cercle(cercle_7))
-canvas.tag_bind(cercle_8, "<1>", lambda cercle: couleur_cercle(cercle_8))
-canvas.tag_bind(cercle_9, "<1>", lambda cercle: couleur_cercle(cercle_9))
+canvas.tag_bind(cercle_1, "<1>", lambda cercle: couleur_cercle(cercle_1, 0, 0))
+canvas.tag_bind(cercle_2, "<1>", lambda cercle: couleur_cercle(cercle_2, 0, 1))
+canvas.tag_bind(cercle_3, "<1>", lambda cercle: couleur_cercle(cercle_3, 0, 2))
+canvas.tag_bind(cercle_4, "<1>", lambda cercle: couleur_cercle(cercle_4, 1, 0))
+canvas.tag_bind(cercle_5, "<1>", lambda cercle: couleur_cercle(cercle_5, 1, 1))
+canvas.tag_bind(cercle_6, "<1>", lambda cercle: couleur_cercle(cercle_6, 1, 2))
+canvas.tag_bind(cercle_7, "<1>", lambda cercle: couleur_cercle(cercle_7, 2, 0))
+canvas.tag_bind(cercle_8, "<1>", lambda cercle: couleur_cercle(cercle_8, 2, 1))
+canvas.tag_bind(cercle_9, "<1>", lambda cercle: couleur_cercle(cercle_9, 2, 2))
 
-canvas.grid()
+boutton_joueur = tk.Button(racine, text = "HUMAIN VS IA")
+boutton_robot = tk.Button(racine, text = "IA VS IA")
+
+boutton_joueur.grid(column = 0, row = 2)
+boutton_robot.grid(row = 3)
 
 racine.mainloop()
