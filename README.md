a2174b820cf1b1a2fbb22ed3fd03b918696c823a
clement coste <clement.coste.rouet@free.fr>
2021-05-24 23:30:06 +0200

    dernière version

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# tapatan.py
+# Projet Tapatan TD7
+## SOMMAIRE
+- Présentation du groupe
+- Présentation du projet
+    * Déroulement du jeu
+    * Fonctionnalité requise
+- Problème rencontré
+- Explication et ensemble du projet
+    * Fonctionnalité réussite et ajouté
+    * Fonctionnalité non réussite
+
+
+# Présentation du groupe
+## Présentation du groupe
+Groupe TD7 :
+
+Sellier Benjamin
+
+Laurence Maëva
+
+Le Goueff Tom
+
+Ghalayini Mohamad
+
+Kbayli Yanis
+
+Coste Clement
+
+# Présentation du projet
+## Déroulement du jeu
+Chacun des deux joueurs dispose de 3 jetons. Le vainqueur est le premier joueur à aligner 3 de ses jetons sur un plateau constitué de 3 lignes horizontales, 3 lignes verticales et 2 lignes en diagonales.
+
+Le jeu ce déroule en deux étapes:
+- Dans un premier temps nous avons le posisionnement des pions qui se fait à tour de rôle sur un plateau vide.
+- Puis nous avons le déplacement qui se fait à tour de rôle, si nous n'avons pas de vainqueur lors du posisionnement.
+
+## Fonctionnalité requise
+
+- Notre interface graphique doit permettre à 2 humains de jouer au jeu. 
+- Faire une version ou chaque victoire rapporte un point et le jeu s’arrête dès qu’un des joueurs à 3 points et est alors déclaré vainqueur.
+- A chaque partie, il faut changer le joueur qui débute la partie. Un match nul n’apporte aucun point.
+
+# Problème rencontré
+
+Lors de la mise en place du groupe qui nous a était donné. Nous avons remarqué qu'il nous manqué déjà deux presonnes (Sellier Benjamin et Ghalayini Mohamad). Cela ne nous a pas facilité la tâche car Sellier Benjamin était censé être le meilleur du groupe, ce qui nous a ralentit dans la programmation mais aussi dans la gestion du travail.
+
+# Explication et ensemble du projet
+## Fonctionnalité réussite :
+- ## Posisionnement des pions 
+    - Pour poser un pion il vous suffit juste de cliquer sur un des ronds noir qui changera de couleur en fonction du premier qui commence. La première personne aura une couleur bleu et la deuxiéme une couleur rouge.
+- ## Déplacement des pions
+    - Pour le déplacement il vous suffira de cliquer sur le pion que vous voulez deplacer,  puis sur un rond noir afin que le déplacement s'effectue. Il vous sera impossible de déplacer un pion sur un autre pion, mais aussi vous pouvez déplacer un pion que de un rond.
+- ## Compteur de victoire
+    - Chaque victoire vous sera affiché
+- ## Afficchage du joueur qui commence
+    - Le rond en haut à droite permet d'indiquer le joueur qui commence. Quand on pose un pion il changera de couleur pour montrer le prochain joueur qui pose ou déplace.
+
+
+## Fonctionnalité non réussite :
+- La fonctionnalité consitant à dire qu'un match est nul lorsque l’on tombe une troisième fois sur la même disposition des jetons sur le plateau n'a pas pu être réalisé.
+- Nous n'avons pas pu faire de fonctionnalité avancé.
+
+
--- tapatan.py
+++ tapatan.py
@@ -1,123 +1,259 @@
+####################################################
+# groupe MPCI 7
+# Clément Coste
+# Tom Le Goueff
+# Yanis Kbayli
+# Maeva Laurence
+# Ghalayini Mohamad
+# Sellier Benjamin
+# https://github.com/uvsq22008945/tapatan.py.git
+####################################################
+
 import tkinter as tk
 import tkinter.messagebox
 
 ###########################
 # constantes du programme
-HAUTEUR = 600
+HAUTEUR = 550
 LARGEUR = 600
 HAUTEUR_LIGNE = 400
 LARGEUR_LIGNE = 400
 RAYON = 10
 cpt = 0
 color = ['red', 'blue']
-compteur = 0
-
-liste = [
-    ['black', "black", "black"],
-    ['black', "black", "black"],
-    ['black', "black", "black"]
-]
-
-liste_deplacement = [[], [], [], [], [], [], [], [], []]
+score_blue = 0
+score_red = 0
 
 
 ##########################
 # fonctions du programme
+def initialiser_le_jeu():
+    """
+    Permet d'initialer le début du jeu.
+    """
+
+    global cercle_1, cercle_2, cercle_3, cercle_4, cercle_5
+    global cercle_6, cercle_7, cercle_8, cercle_9, pion
+    global compteur, success, pion_controlling, couleur_pion
+    global liste, liste_deplacement, position
+
+    if score_blue != 3 and score_red != 3:
+        compteur = 0
+        success = False
+        pion_controlling = None
+        couleur_pion = "black"
+        position = None
+
+        liste = [['black', "black", "black"],
+                 ['black', "black", "black"],
+                 ['black', "black", "black"]]
+
+        liste_deplacement = [[], [], [], [], [], [], [], [], []]
+        cercle_1 = canvas.create_oval(100 - RAYON, 100 - RAYON,
+                                      100 + RAYON, 100 + RAYON, fill="black")
+        cercle_2 = canvas.create_oval(300 - RAYON, 100 - RAYON,
+                                      300 + RAYON, 100 + RAYON, fill="black")
+        cercle_3 = canvas.create_oval(500 - RAYON, 100 - RAYON,
+                                      500 + RAYON, 100 + RAYON, fill="black")
+        cercle_4 = canvas.create_oval(100 - RAYON, 300 - RAYON,
+                                      100 + RAYON, 300 + RAYON, fill="black")
+        cercle_5 = canvas.create_oval(300 - RAYON, 300 - RAYON,
+                                      300 + RAYON, 300 + RAYON, fill="black")
+        cercle_6 = canvas.create_oval(500 - RAYON, 300 - RAYON,
+                                      500 + RAYON, 300 + RAYON, fill="black")
+        cercle_7 = canvas.create_oval(100 - RAYON, 500 - RAYON,
+                                      100 + RAYON, 500 + RAYON, fill="black")
+        cercle_8 = canvas.create_oval(300 - RAYON, 500 - RAYON,
+                                      300 + RAYON, 500 + RAYON, fill="black")
+        cercle_9 = canvas.create_oval(500 - RAYON, 500 - RAYON,
+                                      500 + RAYON, 500 + RAYON, fill="black")
+        pion = canvas.create_oval(20, 20, 50, 50, fill="blue")
+
+        canvas.tag_bind(cercle_1, "<1>",
+                        lambda cercle: couleur_cercle(cercle_1, 0, 0))
+        canvas.tag_bind(cercle_2, "<1>",
+                        lambda cercle: couleur_cercle(cercle_2, 0, 1))
+        canvas.tag_bind(cercle_3, "<1>",
+                        lambda cercle: couleur_cercle(cercle_3, 0, 2))
+        canvas.tag_bind(cercle_4, "<1>",
+                        lambda cercle: couleur_cercle(cercle_4, 1, 0))
+        canvas.tag_bind(cercle_5, "<1>",
+                        lambda cercle: couleur_cercle(cercle_5, 1, 1))
+        canvas.tag_bind(cercle_6, "<1>",
+                        lambda cercle: couleur_cercle(cercle_6, 1, 2))
+        canvas.tag_bind(cercle_7, "<1>",
+                        lambda cercle: couleur_cercle(cercle_7, 2, 0))
+        canvas.tag_bind(cercle_8, "<1>",
+                        lambda cercle: couleur_cercle(cercle_8, 2, 1))
+        canvas.tag_bind(cercle_9, "<1>",
+                        lambda cercle: couleur_cercle(cercle_9, 2, 2))
+    else:
+        if score_blue == score_red:
+            message = "Le match est en égalité"
+        elif score_blue > score_red:
+            message = "Le pion bleu a gagné"
+        else:
+            message = "Le pion rouge a gagné"
+        tkinter.messagebox.showinfo("success", message)
+
+
 def couleur_cercle(cercle, x, y):
-    global color, cpt, liste, compteur
-    if liste[x][y] == "black" and compteur < 6:
+    """
+    La fonction permet de définir le nombre de pion à poser qui est de 6 et
+    permet le déplacement une fois la limite dépassée. Ajoute un message box
+    à chaque victoire et renouvelle le score lors d'une victoire.
+    """
+
+    global color, cpt, liste, compteur, success
+    global pion_controlling, couleur_pion, position
+    global score_red, score_blue
+    global liste_deplacement
+
+    compteur += 1
+    if liste[x][y] == "black" and compteur <= 6 and not success:
         cpt = (cpt + 1) % 2
         canvas.itemconfig(cercle, fill=color[cpt])
         liste[x][y] = color[cpt]
-        if tableau() and compteur != 6:
+        canvas.itemconfig(pion, fill=color[(cpt + 1) % 2])
+        if tableau():
             gagnant = liste[x][y]
             tkinter.messagebox.showinfo("success", gagnant+" Success unlocked")
-        compteur += 1
+            success = True
+            if gagnant == "red":
+                score_red += 1
+            else:
+                score_blue += 1
+            c_zone.itemconfig(c_zone_compteur,
+                              text=f"victoire\n  {score_blue} - {score_red}")
+            initialiser_le_jeu()
     if compteur == 6:
-        deplacement()
-        print(liste_deplacement)
+        deplacement_possible()
+    if compteur >= 6 and not success:
+        if liste[x][y] == color[(cpt + 1) % 2]:
+            pion_controlling, position = cercle, x * 3 + y
+            couleur_pion = liste[x][y]
+        else:
+            if pion_controlling is not None:
+                if x * 3 + y + 1 in liste_deplacement[position]:
+                    canvas.itemconfig(pion_controlling, fill="black")
+                    canvas.itemconfig(cercle, fill=couleur_pion)
+
+                    liste[position // 3][position % 3] = "black"
+                    liste[x][y] = couleur_pion
+                    liste_deplacement = [[], [], [], [], [], [], [], [], []]
+                    deplacement_possible()
+                    pion_controlling = None
+
+                    if tableau():
+                        gagnant = liste[x][y]
+                        tkinter.messagebox.showinfo(
+                                "success", gagnant + " Success unlocked"
+                        )
+                        success = True
+                        if gagnant == "red":
+                            score_red += 1
+                        else:
+                            score_blue += 1
+                        c_zone.itemconfig(
+                                c_zone_compteur,
+                                text=f"victoire\n  {score_blue} - {score_red}"
+                        )
+                        initialiser_le_jeu()
+
+                    if liste[x][y] == couleur_pion:
+                        cpt = (cpt + 1) % 2
+                        canvas.itemconfig(pion, fill=color[(cpt + 1) % 2])
 
 
 def tableau():
-
-    if (    liste[0][0] == liste[0][1] == liste[0][2] != 'black' or # première ligne
-            liste[1][0] == liste[1][1] == liste[1][2] != 'black' or # deuxième ligne
-            liste[2][0] == liste[2][1] == liste[2][2] != 'black' or # troisième ligne
-            liste[0][0] == liste[1][0] == liste[2][0] != 'black' or # première colonne
-            liste[0][1] == liste[1][1] == liste[2][1] != 'black' or # deuxième colonne
-            liste[0][2] == liste[1][2] == liste[2][2] != 'black' or # troisième colonne
-            liste[0][0] == liste[1][1] == liste[2][2] != 'black' or # première diagonale
-            liste[2][0] == liste[1][1] == liste[0][2] != 'black' # deuxième diagonale
-        ): 
+    """
+    Cette fonction permet de reconnaître une victoire.
+    """
+    if (
+        liste[0][0] == liste[0][1] == liste[0][2] != 'black' or
+        liste[1][0] == liste[1][1] == liste[1][2] != 'black' or
+        liste[2][0] == liste[2][1] == liste[2][2] != 'black' or
+        liste[0][0] == liste[1][0] == liste[2][0] != 'black' or
+        liste[0][1] == liste[1][1] == liste[2][1] != 'black' or
+        liste[0][2] == liste[1][2] == liste[2][2] != 'black' or
+        liste[0][0] == liste[1][1] == liste[2][2] != 'black' or
+        liste[2][0] == liste[1][1] == liste[0][2] != 'black'
+    ):
         return True
     return False
 
 
-def deplacement():
+def deplacement_possible():
+    """
+    Permet de reconnaître les déplacements possibles.
+    """
 
     if liste[0][0] != "black":
         deplacement_append(
-            0, 0, 
+            0, 0,
             (liste[0][1], 2),
             (liste[1][0], 4),
         )
     if liste[0][1] != "black":
-            deplacement_append(
-            0, 1, 
+        deplacement_append(
+            0, 1,
             (liste[0][0], 1),
             (liste[0][2], 3),
         )
     if liste[0][2] != "black":
-            deplacement_append(
-            0, 2, 
+        deplacement_append(
+            0, 2,
             (liste[0][1], 2),
-            (liste[0][2], 3),
+            (liste[1][2], 6),
         )
     if liste[1][0] != "black":
-            deplacement_append(
-            1, 0, 
+        deplacement_append(
+            1, 0,
             (liste[0][0], 1),
             (liste[2][0], 7),
         )
     if liste[1][1] != "black":
-            deplacement_append(
-            1, 1, 
+        deplacement_append(
+            1, 1,
             (liste[0][0], 1),
             (liste[0][1], 2),
             (liste[0][2], 3),
             (liste[1][0], 4),
-            (liste[1][0], 6),
+            (liste[1][2], 6),
             (liste[2][0], 7),
             (liste[2][1], 8),
             (liste[2][2], 9)
         )
     if liste[1][2] != "black":
-            deplacement_append(
-            1, 2, 
+        deplacement_append(
+            1, 2,
             (liste[0][2], 3),
             (liste[2][2], 9)
         )
     if liste[2][0] != "black":
-            deplacement_append(
-            2, 0, 
+        deplacement_append(
+            2, 0,
             (liste[1][0], 4),
-            (liste[2][0], 7)
+            (liste[2][1], 8)
         )
     if liste[2][1] != "black":
-            deplacement_append(
-            2, 1, 
+        deplacement_append(
+            2, 1,
             (liste[2][0], 7),
             (liste[2][2], 9)
         )
     if liste[2][2] != "black":
-            deplacement_append(
-            2, 2, 
+        deplacement_append(
+            2, 2,
             (liste[1][2], 6),
             (liste[2][1], 8),
         )
 
 
 def deplacement_append(i, j, *args):
+    """
+    Vérifie le code couleur des pions pour les déplacer.
+    """
     for argument in args:
         if argument[0] == "black":
             liste_deplacement[i*3+j].append(argument[1])
@@ -125,14 +261,21 @@ def deplacement_append(i, j, *args):
         liste_deplacement[i*3+j].append(5)
 
 
-
 #############################
 # programme principal
 racine = tk.Tk()
 canvas = tk.Canvas(racine, width=LARGEUR, height=HAUTEUR)
-canvas.grid()
 
 # plateau
+t_zone = tk.Canvas(racine, bg="white", height=50, width=600)
+message = """
+\nChacun des deux joueurs dispose de 3 jetons.
+Le vainqueur est le premier joueur à aligner 3 de ses jetons sur le plateau.
+"""
+t_zone_Texte = t_zone.create_text(300, 20, text=message)
+c_zone = tk.Canvas(racine, bg="white", height=50, width=600)
+c_zone_compteur = c_zone.create_text(300, 20, text="victoire\n  0 - 0")
+
 canvas.create_rectangle(100, 100, 500, 500, fill="#D3D2A5")
 
 ligne_verticale = canvas.create_line(300, 100, 300, 500, fill="black")
@@ -141,30 +284,10 @@ ligne_horizontale = canvas.create_line(100, 300, 500, 300)
 diagonal = canvas.create_line(100, 100, 500, 500)
 diagonal = canvas.create_line(100, 500, 500, 100)
 
-cercle_1 = canvas.create_oval(100 - RAYON, 100 - RAYON, 100 + RAYON, 100 + RAYON, fill="black")
-cercle_2 = canvas.create_oval(300 - RAYON, 100 - RAYON, 300 + RAYON, 100 + RAYON, fill="black")
-cercle_3 = canvas.create_oval(500 - RAYON, 100 - RAYON, 500 + RAYON, 100 + RAYON, fill="black")
-cercle_4 = canvas.create_oval(100 - RAYON, 300 - RAYON, 100 + RAYON, 300 + RAYON, fill="black")
-cercle_5 = canvas.create_oval(300 - RAYON, 300 - RAYON, 300 + RAYON, 300 + RAYON, fill="black")
-cercle_6 = canvas.create_oval(500 - RAYON, 300 - RAYON, 500 + RAYON, 300 + RAYON, fill="black")
-cercle_7 = canvas.create_oval(100 - RAYON, 500 - RAYON, 100 + RAYON, 500 + RAYON, fill="black")
-cercle_8 = canvas.create_oval(300 - RAYON, 500 - RAYON, 300 + RAYON, 500 + RAYON, fill="black")
-cercle_9 = canvas.create_oval(500 - RAYON, 500 - RAYON, 500 + RAYON, 500 + RAYON, fill="black")
+initialiser_le_jeu()
 
-canvas.tag_bind(cercle_1, "<1>", lambda cercle: couleur_cercle(cercle_1, 0, 0))
-canvas.tag_bind(cercle_2, "<1>", lambda cercle: couleur_cercle(cercle_2, 0, 1))
-canvas.tag_bind(cercle_3, "<1>", lambda cercle: couleur_cercle(cercle_3, 0, 2))
-canvas.tag_bind(cercle_4, "<1>", lambda cercle: couleur_cercle(cercle_4, 1, 0))
-canvas.tag_bind(cercle_5, "<1>", lambda cercle: couleur_cercle(cercle_5, 1, 1))
-canvas.tag_bind(cercle_6, "<1>", lambda cercle: couleur_cercle(cercle_6, 1, 2))
-canvas.tag_bind(cercle_7, "<1>", lambda cercle: couleur_cercle(cercle_7, 2, 0))
-canvas.tag_bind(cercle_8, "<1>", lambda cercle: couleur_cercle(cercle_8, 2, 1))
-canvas.tag_bind(cercle_9, "<1>", lambda cercle: couleur_cercle(cercle_9, 2, 2))
-
-boutton_joueur = tk.Button(racine, text = "HUMAIN VS IA")
-boutton_robot = tk.Button(racine, text = "IA VS IA")
-
-boutton_joueur.grid(column = 0, row = 2)
-boutton_robot.grid(row = 3)
+canvas.grid(row=1, column=0)
+t_zone.grid(row=0, column=0)
+c_zone.grid(row=3, column=0)
 
 racine.mainloop()
